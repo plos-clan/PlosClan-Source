57b04d315a7ba1fec390a260ed2807807fed95a6
wenxuanjun <wenxuanjun@yahoo.com>
2026-04-17 00:26:18 +0800

    Disable prefetch because it cause 503 error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  prefetch: true,
   vite: {
     plugins: [tailwindcss()]
   }
